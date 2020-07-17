21e09d52fbdff7c8befdac377c2e13a6e0f0d4ae
unknown <garimade3@gmail.com>
2020-07-18 00:39:49 +0530

    Added viewset methods

--- profiles_api/profiles_app/views.py
+++ profiles_api/profiles_app/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response #Used to return responses from APIView
 from rest_framework import status
 from profiles_app import serializers
-
+from rest_framework import viewsets
 
 class HelloApiView(APIView):
     """Test APIView"""
@@ -45,3 +45,43 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete object from database"""
         return Response({'method':'delete'})
+
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializer
+    def list(self, request):
+        """Return hello message"""
+        a_viewset = ['a','b','c','d','e']
+
+        return Response({'message': 'Hello', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """Create new hello msg"""
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, 
+            status= status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """Handle getting object by its id"""
+        return Response({'http_method':'GET'})
+
+    def update(self, request, pk=None):
+        """Updatea an object"""
+        return Response({'http_method':'PUT'})
+
+    def update_partial(self, request, pk=None):
+        """Handle partial updating of an object"""
+        serializer = self.serializer_class(data = request.data)
+        return Response({'http_method':'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Removing an object"""
+        return Response({'http_method':'DELETE'})
